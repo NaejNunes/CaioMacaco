ec84805ad2041257a8b37fb7ac1fad605dae9cd5
NaejNunes <jeannobreganunes@hotmail.com>
2021-05-24 16:35:32 -0300

    Sistema de pulo e morte finalizado

---
+++ CaioMacacoUnity/Assets/Scripts/GalhoControle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalhoControle : MonoBehaviour
+{
+    private float timeDestroy = 6;
+    private bool checkGalho = false;
+    public void FixedUpdate()
+    {
+        if (checkGalho == true)
+            timeDestroy -= Time.deltaTime;
+
+        if (timeDestroy <= 3)
+            GetComponent<Animator>().SetBool("QuebrarGalho", true);
+
+        if (timeDestroy <= 0)
+            Destroy(this.gameObject);
+        
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            checkGalho = true;
+            timeDestroy = 6;
+        }
+    }
+}
